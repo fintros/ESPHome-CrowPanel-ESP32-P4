b80c60cceecb3dd045d7a639aee65faa3a9310d1
Alexey Spirkov <dev@alsp.net>
2026-04-22 10:51:43 +0300

    Adds P4 v1.1 version support

--- components/remote_webview/remote_webview_config.h
+++ components/remote_webview/remote_webview_config.h
@@ -4,14 +4,14 @@ namespace esphome {
 namespace remote_webview {
 namespace cfg {
 
-inline constexpr int decode_task_stack = 32 * 1024;
-inline constexpr int ws_task_stack = 8 * 1024;
+inline constexpr int decode_task_stack = 64 * 1024;
+inline constexpr int ws_task_stack = 16 * 1024;
 inline constexpr int ws_task_prio = 5;
-inline constexpr int decode_queue_depth = 12;
+inline constexpr int decode_queue_depth = 16;
 
 inline constexpr size_t ws_max_message_bytes = 64 * 1024;
 inline constexpr size_t ws_buffer_size = 30 * 1024;
-inline constexpr size_t ws_keepalive_interval_us = 60 * 1000 * 1000;
+inline constexpr size_t ws_keepalive_interval_us = 30 * 1000 * 1000;
 
 inline constexpr bool coalesce_moves = true;
 inline constexpr uint32_t move_rate_hz = 60;
